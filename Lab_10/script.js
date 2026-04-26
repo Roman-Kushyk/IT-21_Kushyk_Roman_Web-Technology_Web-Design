a82670670c2f978c728071610ef232fa121e36dd
Kushyk Roman <rkushyk2021@itcollege.lviv.ua>
2026-04-26 16:00:36 +0300

    change

--- Lab_10/script.js
+++ Lab_10/script.js
@@ -1,4 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
+   
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    const savedTheme = localStorage.getItem('theme');
+    
+    if (savedTheme === 'dark') {
+        body.classList.add('dark-theme');
+        themeToggleBtn.textContent = 'Денний режим';
+    }
+
+    themeToggleBtn.addEventListener('click', () => {
+        body.classList.toggle('dark-theme');
+        
+        if (body.classList.contains('dark-theme')) {
+            localStorage.setItem('theme', 'dark');
+            themeToggleBtn.textContent = 'Денний режим';
+        } else {
+            localStorage.setItem('theme', 'light');
+            themeToggleBtn.textContent = 'Нічний режим';
+        }
+    });
+
+  
     const form = document.getElementById('contactForm');
 
     form.addEventListener('submit', function(event) {
@@ -10,12 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (name === '' || email === '' || message === '') {
             alert('Будь ласка, заповніть усі поля!');
-            return; // Зупиняємо виконання функції, якщо є порожні поля
+            return; 
         }
 
-        alert(`Дякую, ${name}! Ваше повідомлення успішно оброблено.`);
+        alert(`Дякую, ${name}! Ваше повідомлення успішно надіслано.\nЯ відповім вам на ${email}.`);
 
-        console.log('--- Нове повідомлення ---');
+        console.log('--- Нове повідомлення з форми ---');
         console.log('Ім\'я:', name);
         console.log('Email:', email);
         console.log('Повідомлення:', message);
